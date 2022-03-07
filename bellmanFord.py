cf10bca18607174dcab9b4d260d70e8696827e4b
Shekhar Reddy Mittapelly <shekharreddymittapelly@MacBookAir1.local>
2022-03-06 18:14:43 -0600

    completed bellmanford

--- bellmanFord.py
+++ bellmanFord.py
@@ -1,24 +1,67 @@
 # Bellman Ford Algorithm in Python
 import os
 import psutil
+import shutil
+import json
+import time
+
+# r = {'is_claimed': 'True', 'rating': 3.5}
+# r = json.dumps(r)
+# loaded_r = json.loads(r)
+# loaded_r['rating'] #Output 3.5
+# type(r) #Output str
+# type(loaded_r) #Output dict
 
 class Graph:
+    data=[{}]
 
     def __init__(self, vertices):
         self.V = vertices   # Total number of vertices in the graph
         self.graph = []     # Array of edges
 
+    def insert(self,originalfile,string):
+        with open(originalfile,'r') as f:
+            with open('./data/newbdata.json','w') as f2: 
+                f2.write(string)
+                f2.write(f.read())
+        
+    def osinfo(self, p):
+        
+        # print("process cpu util in percentage is while in for loop : ",p.cpu_times()._asdict())
+        # print("process cpu util excluding the IO/Blockstate while in for loop : ",round(sum(p.cpu_times()[:2]),2))
+        self.data[0]['cpu times'] = round(sum(p.cpu_times()[:2]),2)
+        #print("process memory in percentage is while in for loop : ", p.memory_info() )
+        #print("process cpu_percent in percentage is : ", p.cpu_percent(interval=1.0) )
+        #print("process IO counters : ", p.io_counters())
+        #print("Number of cpu s : ", p.cpu_num() )
+        #print("process number of context switches is : ", p.num_ctx_switches()._asdict()['voluntary'] )
+        self.data[0]['voluntary ctx switch'] = p.num_ctx_switches()._asdict()['voluntary']
+        #print("process memory_full info : ", p.memory_full_info() )
+        #print("process RSS(aka Resident Set Size) memory in percentage is : ", round(p.memory_percent(memtype="rss"),2))
+        self.data[0]['RSS'] = round(p.memory_percent(memtype="rss"),2)
+        #print("process VMS(aka Virtual Memory Size) memory in percentage is : ", round(p.memory_percent(memtype="vms"),2))
+        self.data[0]['VMS'] = round(p.memory_percent(memtype="vms"),2)
+        self.data[0]['Pgfaults'] = round(p.memory_percent(memtype="pfaults"),2)
+        
+        #print("process USS(aka Unique Set Size) memory in percentage is : ", round(p.memory_percent(memtype="uss"),2))
+        self.data[0]['memory%'] = round(p.memory_percent(memtype="uss"),2)
+
+        #print("process memory maps : ", p.memory_maps() )
+        #print("Number of open files : ", p.open_files() )
+        #print("=====================================================================")
+        #print("process memory in percentage is : ",  )
+        total, used, free = shutil.disk_usage("/")
+        self.data[0]["HDUsage"]= (used // (2**30))
+        
+        
+
     # Add edges
     def add_edge(self, s, d, w):
         self.graph.append([s, d, w])
 
-    # Print the solution
-    # def print_solution(self, dist):
-    #     print("Vertex Distance from Source")
-    #     for i in range(self.V):
-    #         print("{0}\t\t{1}".format(i, dist[i]))
-
+    
     def bellman_ford(self, src, p):
+        start_time = time.time()
         print("================================== START OF THE FUNCTION ===================================")
 
         # Step 1: fill the distance array and predecessor array
@@ -31,22 +74,6 @@ class Graph:
             for s, d, w in self.graph:
                 if dist[s] != float("Inf") and dist[s] + w < dist[d]:
                     dist[d] = dist[s] + w
-            print("process cpu util in percentage is while in for loop : ",p.cpu_times())
-            print("process cpu util excluding the IO/Blockstate while in for loop : ",sum(p.cpu_times()[:2]))
-            print("process memory in percentage is while in for loop : ", p.memory_info() )
-            print("process cpu_percent in percentage is : ", p.cpu_percent(interval=1.0) )
-            #print("process IO counters : ", p.io_counters())
-            #print("Number of cpu s : ", p.cpu_num() )
-            print("process number of context switches is : ", p.num_ctx_switches() )
-            print("process memory_full info : ", p.memory_full_info() )
-            print("process RSS(aka Resident Set Size) memory in percentage is : ", p.memory_percent(memtype="rss"))
-            print("process VMS(aka Virtual Memory Size) memory in percentage is : ", p.memory_percent(memtype="vms"))
-            print("process USS(aka Unique Set Size) memory in percentage is : ", p.memory_percent(memtype="uss"))
-            #print("process memory maps : ", p.memory_maps() )
-            print("Number of open files : ", p.open_files() )
-            print("=====================================================================")
-            #print("process memory in percentage is : ",  )
-
             
         # Step 3: detect negative cycle
         # if value changes then we have a negative cycle in the graph
@@ -55,17 +82,16 @@ class Graph:
             if dist[s] != float("Inf") and dist[s] + w < dist[d]:
                 print("Graph contains negative weight cycle")
                 return
-        
-        print("================================== END OF THE FUNCTION ===================================")
-        print("process cpu util in percentage is : ",p.cpu_times())
-        print("process memory in percentage is : ", p.memory_info() )
-        print("process memory in percentage is : ", p.cpu_percent(interval=1.0) )
-        
-
-        # No negative weight cycle found!
-        # Print the distance and predecessor array
-        # self.print_solution(dist)
-
+        self.osinfo(p)
+        # print("================================== END OF THE FUNCTION ===================================")
+        # print("process cpu util in percentage is : ",p.cpu_times())
+        # print("process memory in percentage is : ", p.memory_info() )
+        # print("process memory in percentage is : ", p.cpu_percent(interval=1.0) )
+        # print("process USS(aka Unique Set Size) memory in percentage is : ", p.memory_percent(memtype="uss"))
+        timetaken = (time.time() - start_time)
+        self.data[0]['timetaken'] = round(timetaken,6)
+        with open('./data/bdata.json', 'w') as f:
+            json.dump(self.data, f)
 
 g = Graph(5)
 g.add_edge(0, 1, 5)
@@ -81,7 +107,7 @@ print("================================== BEFORE START OF THE PROGRAM ==========
 print("process cpu util in percentage is : ",p.cpu_times())
 print("process memory in percentage is : ", p.memory_info() )
 print("process memory in percentage is : ", p.cpu_percent(interval=1.0) )
-import time
+
 start_time = time.time()
 g.bellman_ford(0,p)
 print("================================== TOTAL RUNNING TIMEPROGRAM ===================================")
@@ -89,3 +115,10 @@ print("--- %s seconds ---" % (time.time() - start_time))
 print("================================== END OF THE PROGRAM ===================================")
 
 
+total, used, free = shutil.disk_usage("/")
+
+print("Total: %d GiB" % (total // (2**30)))
+print("Used: %d GiB" % (used // (2**30)))
+print("Free: %d GiB" % (free // (2**30)))
+
+g.insert("./data/bdata.json", "data=")
